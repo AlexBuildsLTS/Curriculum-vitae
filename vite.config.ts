e6030ce642a693a8774aaf48c546f5072854e17e
Alex Youssef <alex.youssef@live.com>
2025-01-13 10:49:33 +0200

    updated viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig as viteDefineConfig } from 'vite';
-export default viteDefineConfig({
+
+export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    outDir: 'dist',
   },
 });
